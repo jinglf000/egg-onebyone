080afdb80d9983edc4ce2d314d1bc6f4bb80ab7f
jinglf000 <jinglongfei@jsb5.com>
2018-05-11 18:10:32 +0800

    egg loader quence

--- app/controller/news.js
+++ app/controller/news.js
@@ -2,21 +2,13 @@ const Controller = require('egg').Controller;
 
 class NewsController extends Controller {
   async list() {
-    const dataList = {
-      list: [{
-          id: 1,
-          title: 'Hack news 1',
-          url: '/news/1'
-        },
-        {
-          id: 2,
-          title: 'Hack news 2',
-          url: '/news/2'
-        }
-      ]
-    };
+    const ctx = this.ctx;
+    const page = ctx.query.page || 1;
+    const newsList = await ctx.service.news.list(page)
 
-    await this.ctx.render('news/list.tpl', dataList);
+    await this.ctx.render('news/list.tpl', {
+      list: newsList
+    });
 
   }
 }
